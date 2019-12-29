5dc4b3992d25f89505afd237f23c6a52ff91e301
Lalit-tech <55653502+Lalit-tech@users.noreply.github.com>
2019-12-29 23:47:17 +0530

    Add files via upload

---
+++ odd program .c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+   int num;
+
+   printf("Enter an integer: ");
+   scanf("%d",&num);
+
+   // Modulus (%) returns remainder
+   if ( num%2 == 0 )
+      printf("%d is an even number", num);
+   else
+      printf("%d is an odd number", num);
+
+   return 0;
+}
